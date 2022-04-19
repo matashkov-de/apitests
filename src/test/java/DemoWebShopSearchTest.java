5f9206c8cfe650aeb7c9e2c6d3438311663fd4fc
matashkov-de <mat.dmitry539@gmail.com>
2022-04-19 11:06:17 +0500

    allure, credentials and logs added

--- src/test/java/DemoWebShopSearchTest.java
+++ src/test/java/DemoWebShopSearchTest.java
@@ -1,9 +1,45 @@
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import models.RegCredentials;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 public class DemoWebShopSearchTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.filters(new AllureRestAssured());
+    }
+
+    @Test
+    public void registrationSuccessfulTest() {
+        //request: https://reqres.in/api/register
+        //check: Статус 200
+        //check: Вернулся не пустой token
+
+        RegCredentials credentials = new RegCredentials();
+
+        credentials.setEmail("eve.holt@reqres.in");
+        credentials.setPassword("pistol");
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(credentials)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", notNullValue());
+    }
 
     @Test
     void searchForTcpInstructorLedTraining() {
@@ -11,10 +47,13 @@ public class DemoWebShopSearchTest {
         //Проверяем, что предмет найден в поиске
 
         given()
+                .log().uri()
+                .log().body()
                 .when()
                 .get("http://demowebshop.tricentis.com/catalog/searchtermautocomplete?term=TCP Instructor Led Training")
                 .then()
-                .log().all()
+                .log().status()
+                .log().body()
                 .statusCode(200)
                 .body("label", hasItem("TCP Instructor Led Training"));
 
@@ -27,11 +66,15 @@ public class DemoWebShopSearchTest {
 
         given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .log().uri()
+                .log().body()
                 .when()
                 .post("http://demowebshop.tricentis.com/addproducttocart/details/66/2")
                 .then()
-                .log().all()
+                .log().status()
+                .log().body()
                 .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemes/Demowebshop_addToWishlist_Response.json"))
                 .body("success", is(true))
                 .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"))
                 .body("updatetopwishlistsectionhtml", notNullValue());
---
+++ src/test/java/models/RegCredentials.java
@@ -0,0 +1,9 @@
+package models;
+
+import lombok.Data;
+
+@Data
+public class RegCredentials {
+    private String email;
+    private String password;
+}
